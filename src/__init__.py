4923fb8ecd5d8a146b73fad558725a25a1f080a3
Simone Boscaglia <simone.boscaglia@gmail.com>
2025-12-15 14:41:51 +0100

    Added avatar templates, added chat template
    BREAKING CHANGE: deleted old avatar placeholder

--- src/__init__.py
+++ src/__init__.py
@@ -113,10 +113,33 @@ def create_app(test_config=None):
                                description=desc)
 
 
+    messages = [
+        {"type": "out", "value": "a"},
+        {"type": "out", "value": "b"},
+        {"type": "in",  "value": "c"},
+        {"type": "out", "value": "d"},
+        {"type": "in",  "value": "e"},
+        {"type": "out", "value": "f"},
+        {"type": "out", "value": "g"},
+        {"type": "out", "value": "g"},
+        {"type": "out", "value": "g"},
+        {"type": "out", "value": "g"},
+    ]
+
+    players = [
+        {"username" : "Pippo", "MAX_HP": 10, "HP": 10, "percentage": 100},
+        {"username" : "Giacomo", "MAX_HP": 23, "HP": 15, "percentage": 65.2},
+        {"username" : "Anacleto", "MAX_HP": 11, "HP": 8, "percentage": 72.7 },
+        {"username" : "Bimbo", "MAX_HP": 0, "HP": 0, "percentage": 0 }
+    ]
+
+
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return render_template('character_sheet.html', character=test_character)
-
+        # return render_template('landing.html')
+        # return render_template('character_selection.html')
+        # return render_template('character_sheet.html', character=test_character)
+        return render_template('chat.html', messages=messages, players=players)
     return app
 
