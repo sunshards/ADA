4705cfb02e5aa64172974946ce4b8bc78885a699
Simone Boscaglia <simone.boscaglia@gmail.com>
2025-12-16 23:17:54 +0100

    feat: added submit button and post method to landing html template, added post handling to landing.py

--- src/landing.py
+++ src/landing.py
@@ -7,5 +7,13 @@ bp = Blueprint('landing', __name__,)
 
 @bp.route('/', methods=('GET', 'POST'))
 def landing():
-    
+    if request.method == 'POST':
+        desc = None
+        desc = request.form.get('characterPrompt')
+        
+        if not desc:
+            error = 'Description is required.'
+        else:
+            print(desc)
+
     return render_template('landing/landing.html')
