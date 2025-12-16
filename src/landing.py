5319d5c5e40988bd1324ff754274968ab1bae14a
Donato <Kyashan2005@gmail.com>
2025-12-16 23:22:34 +0100

    added basic mongo connection

--- src/landing.py
+++ src/landing.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, current_app
 )
 import pymongo
 
@@ -7,4 +7,5 @@ bp = Blueprint('landing', __name__,)
 
 @bp.route('/')
 def landing():
+    users = current_app.db.users.find()
     return render_template('landing/landing.html')
