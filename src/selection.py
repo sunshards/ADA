3c31fce04022ade20bc2bfbeeb449b3c71b98c7d
Simone Boscaglia <simone.boscaglia@gmail.com>
2025-12-18 16:07:59 +0100

    feat: added database query for character selection

--- src/selection.py
+++ src/selection.py
@@ -1,10 +1,21 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify, current_app
 )
 
 bp = Blueprint('selection', __name__, url_prefix='/selection')
 
 @bp.route('/', methods=('GET', 'POST'))
-def creation():
-    return render_template('character_sheet/character_sheet.html', characters=populated_character)
+def selection():
+    characters_id = g.user["Characters"]
+
+    player_characters = []
+    for char_id in characters_id:
+        char_query = current_app.db['Characters'].find_one({"_id": char_id})
+        if char_query:
+            player_characters.append({
+                "name": char_query["name"],
+                "avatar_base64": char_query["avatar_base64"]
+            })
+    
+    return render_template('character_selection/character_selection.html', characters=player_characters)
 
